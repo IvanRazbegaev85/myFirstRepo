16338a66f0ebf5c9793349422f817a76d6f2e014
Ivan Razbegaev <sinnless13@gmail.com>
2021-09-30 23:40:24 +0300

    added hello world output

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-
-const a = "Переменная из lesson03";
-
-const b = "Наша ветка lesson01";
-
-const logged = () => {
-    console.log("наконец-то мы узнали все команды, необходимые для работы с Git")
-}
+console.log("Hello World!")
